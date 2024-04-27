85948c01741a32a5d8219ad7a1e8afa747c81ac0
Jaeho Yoo <y73447jh@gmail.com>
2024-04-27 23:45:52 +0900

    1. 샘플 프로젝트 구성

--- TabToScrollSample/ContentView.swift
+++ TabToScrollSample/ContentView.swift
@@ -8,17 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+  var body: some View {
+    TabView {
+      RedView()
+        .tabItem {
+          Label("RED", systemImage: "flame")
         }
-        .padding()
+
+      BlueView()
+        .tabItem {
+          Label("BLUE", systemImage: "drop")
+        }
     }
+  }
+}
+
+struct RedView: View {
+
+  var body: some View {
+    NavigationStack {
+      ScrollView {
+        VStack(spacing: .zero) {
+          ForEach(1...100, id: \.self) { num in
+            NavigationLink(destination: RedView()) {
+              Color.red.opacity(Double(num) / 100)
+                .frame(height: 30)
+                .overlay {
+                  if num == 1 {
+                    Text("TOP")
+                  } else if num == 100 {
+                    Text("BOTTOM")
+                  }
+                }
+                .font(.title.bold())
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+struct BlueView: View {
+
+  var body: some View {
+    NavigationStack {
+      ScrollView {
+        VStack(spacing: .zero) {
+          ForEach(1...100, id: \.self) { num in
+            NavigationLink(destination: BlueView()) {
+              Color.blue.opacity(Double(num) / 100)
+                .frame(height: 30)
+                .overlay {
+                  if num == 1 {
+                    Text("TOP")
+                  } else if num == 100 {
+                    Text("BOTTOM")
+                  }
+                }
+                .font(.title.bold())
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
--- TabToScrollSample/TabToScrollSampleApp.swift
+++ TabToScrollSample/TabToScrollSampleApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct TabToScrollSampleApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
