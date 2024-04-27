276d57421d1c3d7491503f2c5f247b5a62a5283c
Jaeho Yoo <y73447jh@gmail.com>
2024-04-27 22:59:02 +0900

    Initial Commit

---
+++ TabToScrollSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabToScrollSample
+//
+//  Created by 유재호 on 4/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabToScrollSample/TabToScrollSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabToScrollSampleApp.swift
+//  TabToScrollSample
+//
+//  Created by 유재호 on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TabToScrollSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
